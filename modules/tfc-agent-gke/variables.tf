gautambaghel/terraform-google-terraform-cloud-agents

commit 5a04f5e54abcb5da90b52c08afaa7c29c6d390c5

renaming conventions and pre-commit hooks
renaming mig-native folder to mig-vm

renaming

modules/tfc-agent-gke/variables.tf
@@ -8,13 +8,13 @@ variable "project_id" {
 variable "region" {
   type        = string
   description = "The GCP region to deploy instances into"
-  default     = "us-west1"
+  default     = "us-central1"
 }
 
 variable "zones" {
   type        = list(string)
   description = "The GCP zone to deploy gke into"
-  default     = ["us-west1-a"]
+  default     = ["us-central1-a"]
 }
 
 variable "ip_range_pods_name" {
@@ -44,7 +44,7 @@ variable "ip_range_services_cider" {
 variable "network_name" {
   type        = string
   description = "Name for the VPC network"
-  default     = "tfc-agent-network"
+  default     = "tfc_agent_network"
 }
 
 variable "subnet_ip" {

modules/tfc-agent-mig-container-vm/main.tf
@@ -1,12 +1,12 @@
 # Copyright (c) HashiCorp, Inc.
 
 locals {
-  dindVolumeMounts = var.dind ? [{
+  dind_volume_mounts = var.dind ? [{
     mountPath = "/var/run/docker.sock"
     name      = "dockersock"
     readOnly  = false
   }] : []
-  dindVolumes = var.dind ? [
+  dind_volumes = var.dind ? [
     {
       name = "dockersock"
 
@@ -14,8 +14,8 @@ locals {
         path = "/var/run/docker.sock"
       }
   }] : []
-  network_name    = var.create_network ? google_compute_network.tfc-agent-network[0].self_link : var.network_name
-  subnet_name     = var.create_network ? google_compute_subnetwork.tfc-agent-subnetwork[0].self_link : var.subnet_name
+  network_name    = var.create_network ? google_compute_network.tfc_agent_network[0].self_link : var.network_name
+  subnet_name     = var.create_network ? google_compute_subnetwork.tfc_agent_subnetwork[0].self_link : var.subnet_name
   service_account = var.service_account == "" ? google_service_account.tfc_agent_service_account[0].email : var.service_account
   instance_name   = "${var.tfc_agent_name_prefix}-${random_string.suffix.result}"
 }
@@ -29,26 +29,26 @@ resource "random_string" "suffix" {
 /*****************************************
   Optional TFC Agent Networking
  *****************************************/
-resource "google_compute_network" "tfc-agent-network" {
+resource "google_compute_network" "tfc_agent_network" {
   count                   = var.create_network ? 1 : 0
   name                    = var.network_name
   project                 = var.project_id
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "tfc-agent-subnetwork" {
+resource "google_compute_subnetwork" "tfc_agent_subnetwork" {
   count         = var.create_network ? 1 : 0
   project       = var.project_id
   name          = var.subnet_name
   ip_cidr_range = var.subnet_ip
   region        = var.region
-  network       = google_compute_network.tfc-agent-network[0].name
+  network       = google_compute_network.tfc_agent_network[0].name
 }
 
 resource "google_compute_router" "default" {
   count   = var.create_network ? 1 : 0
   name    = "${var.network_name}-router"
-  network = google_compute_network.tfc-agent-network[0].self_link
+  network = google_compute_network.tfc_agent_network[0].self_link
   region  = var.region
   project = var.project_id
 }
@@ -88,7 +88,7 @@ resource "google_project_iam_binding" "gce" {
   TFC Agent GCE Instance Template
  *****************************************/
 
-module "gce-container" {
+module "gce_container" {
   source  = "terraform-google-modules/container-vm/google"
   version = "~> 3.0"
   container = {
@@ -124,7 +124,7 @@ module "gce-container" {
         name      = "tempfs-0"
         readOnly  = false
       }
-    ], local.dindVolumeMounts)
+    ], local.dind_volume_mounts)
   }
 
   # Declare the volumes
@@ -136,7 +136,7 @@ module "gce-container" {
         medium = "Memory"
       }
     }
-  ], local.dindVolumes)
+  ], local.dind_volumes)
 
   restart_policy = var.restart_policy
 }
@@ -162,16 +162,16 @@ module "mig_template" {
   source_image_project = "cos-cloud"
   startup_script       = var.startup_script
   name_prefix          = var.tfc_agent_name_prefix
-  source_image         = reverse(split("/", module.gce-container.source_image))[0]
+  source_image         = reverse(split("/", module.gce_container.source_image))[0]
   metadata = merge(var.additional_metadata, {
     google-logging-enabled      = "true"
-    "gce-container-declaration" = module.gce-container.metadata_value
+    "gce_container-declaration" = module.gce_container.metadata_value
   })
   tags = [
     local.instance_name
   ]
   labels = {
-    container-vm = module.gce-container.vm_container_label
+    container-vm = module.gce_container.vm_container_label
   }
 }
 /*****************************************

modules/tfc-agent-mig-container-vm/variables.tf
@@ -8,13 +8,13 @@ variable "project_id" {
 variable "region" {
   type        = string
   description = "The GCP region to deploy instances into"
-  default     = "us-east4"
+  default     = "us-central1"
 }
 
 variable "network_name" {
   type        = string
   description = "Name for the VPC network"
-  default     = "tfc-agent-network"
+  default     = "tfc_agent_network"
 }
 
 variable "create_network" {

modules/tfc-agent-mig-vm/variables.tf
@@ -8,13 +8,13 @@ variable "project_id" {
 variable "region" {
   type        = string
   description = "The GCP region to deploy instances into"
-  default     = "us-west1"
+  default     = "us-central1"
 }
 
 variable "network_name" {
   type        = string
   description = "Name for the VPC network"
-  default     = "tfc-agent-network"
+  default     = "tfc_agent_network"
 }
 
 variable "create_network" {
@@ -133,7 +133,7 @@ variable "tfc_agent_auto_update" {
 variable "tfc_agent_name_prefix" {
   type        = string
   description = "This name may be used in the Terraform Cloud user interface to help easily identify the agent"
-  default     = "tfc-agent-mig-vm"
+  default     = "tfc_agent_mig-vm"
 }
 
 variable "tfc_agent_token" {

examples/tfc-agent-gke-simple/main.tf
@@ -30,7 +30,7 @@ resource "tfe_agent_token" "tfc_agent_token" {
   description   = var.tfc_agent_pool_token
 }
 
-module "tfc-agent-gke" {
+module "tfc_agent_gke" {
   source          = "../../modules/tfc-agent-gke"
   create_network  = true
   project_id      = var.project_id

examples/tfc-agent-gke-simple/variables.tf
@@ -13,7 +13,7 @@ variable "tfc_org_name" {
 variable "tfc_project_name" {
   type        = string
   description = "Terraform Cloud project name to be created"
-  default     = "GCP US West GKE"
+  default     = "GCP Agents GKE"
 }
 
 variable "tfc_workspace_name" {

examples/tfc-agent-mig-container-vm-simple/main.tf
@@ -27,7 +27,7 @@ resource "tfe_agent_token" "tfc_agent_token" {
   description   = var.tfc_agent_pool_token
 }
 
-module "tfc-agent-mig" {
+module "tfc_agent_mig" {
   source          = "../../modules/tfc-agent-mig-container-vm"
   create_network  = true
   project_id      = var.project_id

examples/tfc-agent-mig-container-vm-simple/variables.tf
@@ -13,7 +13,7 @@ variable "tfc_org_name" {
 variable "tfc_project_name" {
   type        = string
   description = "Terraform Cloud project name to be created"
-  default     = "GCP US West VM"
+  default     = "GCP Agents Container VM"
 }
 
 variable "tfc_workspace_name" {

commit d87b5b6492edcec5875b70666b2498a9c8cc4e75

Fix variable defaults (#3)
Fixes a few errors introduced in 5a04f5e

modules/tfc-agent-gke/variables.tf
@@ -44,7 +44,7 @@ variable "ip_range_services_cider" {
 variable "network_name" {
   type        = string
   description = "Name for the VPC network"
-  default     = "tfc_agent_network"
+  default     = "tfc-agent-network"
 }
 
 variable "subnet_ip" {

modules/tfc-agent-mig-container-vm/main.tf
@@ -165,7 +165,7 @@ module "mig_template" {
   source_image         = reverse(split("/", module.gce_container.source_image))[0]
   metadata = merge(var.additional_metadata, {
     google-logging-enabled      = "true"
-    "gce_container-declaration" = module.gce_container.metadata_value
+    "gce-container-declaration" = module.gce_container.metadata_value
   })
   tags = [
     local.instance_name

modules/tfc-agent-mig-container-vm/variables.tf
@@ -14,7 +14,7 @@ variable "region" {
 variable "network_name" {
   type        = string
   description = "Name for the VPC network"
-  default     = "tfc_agent_network"
+  default     = "tfc-agent-network"
 }
 
 variable "create_network" {

modules/tfc-agent-mig-vm/variables.tf
@@ -14,7 +14,7 @@ variable "region" {
 variable "network_name" {
   type        = string
   description = "Name for the VPC network"
-  default     = "tfc_agent_network"
+  default     = "tfc-agent-network"
 }
 
 variable "create_network" {
@@ -133,7 +133,7 @@ variable "tfc_agent_auto_update" {
 variable "tfc_agent_name_prefix" {
   type        = string
   description = "This name may be used in the Terraform Cloud user interface to help easily identify the agent"
-  default     = "tfc_agent_mig-vm"
+  default     = "tfc-agent-mig-vm"
 }
 
 variable "tfc_agent_token" {